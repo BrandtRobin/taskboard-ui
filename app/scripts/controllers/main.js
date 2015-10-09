3d131270d743b30d7d4d87f8d7bf91d018f98dfb
Eric Lantz <elantz12@gmail.com>
2015-10-09 22:19:31 +0200

    Added design and hover trash icon

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,110 +1,110 @@
 'use strict';
 angular.module('taskboardApp')
-  .controller('MainCtrl', function ($scope, workItemDb) {
-    var teamId = 1;
-    $scope.backlog = [];
-    $scope.done = [];
-    $scope.active = [];
+	.controller('MainCtrl', function ($scope, workItemDb) {
+		var teamId = 1;
+		$scope.backlog = [];
+		$scope.done = [];
+		$scope.active = [];
 
-    function addWorkItemToTeam(workItemId) {
-      workItemDb.addToTeam(workItemId, teamId)
-        .then(getWorkItems);
-    }
+		function addWorkItemToTeam(workItemId) {
+			workItemDb.addToTeam(workItemId, teamId)
+				.then(getWorkItems);
+		}
 
-    function getWorkItems() {
-      workItemDb.getAll(teamId)
-        .then(function (res) {
-          $scope.backlog = [];
-          $scope.done = [];
-          $scope.active = [];
-          var workItems = res.data;
-          for (var i = 0; i < workItems.length; i++) {
-            var workItem = workItems[i];
-            if (workItem.status === 'backlog') {
-              $scope.backlog.push(workItem);
-            } else if (workItem.status === 'done') {
-              $scope.done.push(workItem);
-            } else if (workItem.status === 'active') {
-              $scope.active.push(workItem);
-            } else {
-            }
-          }
-        });
-    }
+		function getWorkItems() {
+			workItemDb.getAll(teamId)
+				.then(function (res) {
+					$scope.backlog = [];
+					$scope.done = [];
+					$scope.active = [];
+					var workItems = res.data;
+					for (var i = 0; i < workItems.length; i++) {
+						var workItem = workItems[i];
+						if (workItem.status === 'backlog') {
+							$scope.backlog.push(workItem);
+						} else if (workItem.status === 'done') {
+							$scope.done.push(workItem);
+						} else if (workItem.status === 'active') {
+							$scope.active.push(workItem);
+						} else {}
+					}
+				});
+		}
 
-    function createWorkItem(title, description) {
-      var workItemId;
-      var status = 'backlog';
-      var workItem = {
-        title: title,
-        description: description,
-        status: status
-      };
+		function createWorkItem(title, description) {
+			var workItemId;
+			var status = 'backlog';
+			var workItem = {
+				title: title,
+				description: description,
+				status: status
+			};
 
-      workItemDb.add(workItem)
-        .then(function (res) {
-          workItemId = res.headers('Location').split('id/').pop();
-          addWorkItemToTeam(workItemId);
-        });
-    }
+			workItemDb.add(workItem)
+				.then(function (res) {
+					workItemId = res.headers('Location').split('id/').pop();
+					addWorkItemToTeam(workItemId);
+				});
+		}
 
-    function deleteWorkItem(workItemId) {
-      workItemDb.delete(workItemId);
-    }
+		function deleteWorkItem(workItemId) {
+			workItemDb.delete(workItemId);
+		}
 
-    function addIssueToWorkItem(issueId, workItemId) {
-      workItemDb.addIssue(issueId, workItemId)
-        .then(getWorkItems());
-    }
+		function addIssueToWorkItem(issueId, workItemId) {
+			workItemDb.addIssue(issueId, workItemId)
+				.then(getWorkItems());
+		}
 
-    function addUserToWorkItem(userId, workItemId) {
-      workItemDb.addUser(userId, workItemId)
-        .then(getWorkItems());
-    }
+		function addUserToWorkItem(userId, workItemId) {
+			workItemDb.addUser(userId, workItemId)
+				.then(getWorkItems());
+		}
 
-    function addStatusToWorkItem(status, workItemId) {
-      workItemDb.addStatus(status, workItemId);
-    }
+		function addStatusToWorkItem(status, workItemId) {
+			workItemDb.addStatus(status, workItemId);
+		}
 
-    $scope.sendForm = function (workItemData) {
-      if ($scope.workItemForm.$valid) {
-        createWorkItem($scope.workItemData.title, $scope.workItemData.description);
+		$scope.sendForm = function (workItemData) {
+			if ($scope.workItemForm.$valid) {
+				createWorkItem($scope.workItemData.title, $scope.workItemData.description);
 
-        $('#myModal').modal('hide');
+				$('#myModal').modal('hide');
 
-        $scope.workItemData.title = "";
-        $scope.workItemData.description = "";
-      }
+				$scope.workItemData.title = "";
+				$scope.workItemData.description = "";
+			}
 
-    };
+		};
 
-    $scope.removeActive = function (workItem) {
-      $scope.active.splice($scope.active.indexOf(workItem), 1);
-      workItemDb.delete(workItem.id);
-    };
+		$scope.removeActive = function (workItem) {
+			$scope.active.splice($scope.active.indexOf(workItem), 1);
+			workItemDb.delete(workItem.id);
+		};
 
-    $scope.removeBacklog = function (workItem) {
-      $scope.backlog.splice($scope.backlog.indexOf(workItem), 1);
-      workItemDb.delete(workItem.id);
-    };
+		$scope.removeBacklog = function (workItem) {
+			$scope.backlog.splice($scope.backlog.indexOf(workItem), 1);
+			workItemDb.delete(workItem.id);
+		};
 
-    $scope.removeDone = function (workItem) {
-      $scope.done.splice($scope.done.indexOf(workItem), 1);
-      workItemDb.delete(workItem.id);
-    };
-
-
-    $scope.dragControlListeners = {
-      itemMoved: function (event) {
-        var status = event.dest.sortableScope.element.context.id;
-        var workItemId = event.source.itemScope.workItem.id;
-        
-        addStatusToWorkItem(status, workItemId);
-
-      }
-    };
+		$scope.removeDone = function (workItem) {
+			$scope.done.splice($scope.done.indexOf(workItem), 1);
+			workItemDb.delete(workItem.id);
+		};
 
 
-    getWorkItems();
+		$scope.dragControlListeners = {
+			itemMoved: function (event) {
+				var status = event.dest.sortableScope.element.context.id;
+				console.log(status);
+				var workItemId = event.source.itemScope.workItem.id;
 
-  });
+				addStatusToWorkItem(status, workItemId);
+
+			}
+		};
+
+
+		getWorkItems();
+
+	});
